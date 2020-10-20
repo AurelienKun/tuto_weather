534110764ede0ce3f8f41895b7e795805a4d4e0d
Aurelien <adebaisieux@tp.michelin.com>
2020-10-20 15:05:56 +0200

    FIX: initial project cleanup

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({this.title});
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Wecome to weather app"),
+      ),
+    );
+  }
+}
